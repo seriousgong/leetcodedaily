9a3b0e27dc0d812e7c9a59350fd0c23e6b2b9cfb
gc <chankungdeveloper@gmail.com>
2021-03-05 13:51:23 +0800

    day4 修改

--- year2021/m3/day4.go
+++ year2021/m3/day4.go
@@ -7,27 +7,26 @@ import (
 /*没完全想出来*/
 func maxEnvelopes(envelopes [][]int) int {
 	sort.Slice(envelopes, func(i, j int) bool {
-		return envelopes[i][0] < envelopes[j][0] || envelopes[i][1] < envelopes[j][0]
+		return envelopes[i][0] < envelopes[j][0] || envelopes[i][0] == envelopes[j][0] && envelopes[i][1] < envelopes[j][1]
 	})
-	return loop(envelopes, 0, []int{0, 0})
-}
-
-func loop(envelopes [][]int, curr int, last []int) int {
-	if curr == len(envelopes) {
-		return 0
+	//fmt.Println(envelopes)
+	dp, res := make([]int, len(envelopes)), 0
+	for i := range dp {
+		dp[i] = 1
 	}
-	m1, m2 := 0, 0
-	if envelopes[curr][0] > last[0] && envelopes[curr][1] > last[1] {
-		m1 = max(loop(envelopes, curr+1, envelopes[curr])+1, loop(envelopes, curr+1, envelopes[curr]))
+	for i := 1; i < len(envelopes); i++ {
+		for j := 0; j < i; j++ {
+			if envelopes[i][1] > envelopes[j][1] {
+				if dp[i] < dp[j]+1 {
+					dp[i] = dp[j] + 1
+				}
+			}
+		}
 	}
-	m2 = loop(envelopes, curr+1, envelopes[curr])
-	return max(m1, m2)
-
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
+	for _, val := range dp {
+		if val > res {
+			res = val
+		}
 	}
-	return b
+	return res
 }

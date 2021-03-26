a71c37f0e6dc0888fa9d230387901c76b2fb66ae
gc <chankungdeveloper@gmail.com>
2021-03-26 09:37:01 +0800

    day26

--- year2021/m3/day26.go
+++ year2021/m3/day26.go
@@ -1,6 +1,22 @@
 package m3
 
 func deleteDuplicates(head *ListNode) *ListNode {
+	//return withHeader(head)
+	return withoutHeader(head)
+}
+
+func withoutHeader(head *ListNode) *ListNode {
+	for tmp := head; tmp != nil && tmp.Next != nil; {
+		if tmp.Val == tmp.Next.Val {
+			tmp.Next = tmp.Next.Next
+		} else {
+			tmp = tmp.Next
+		}
+	}
+	return head
+}
+
+func withHeader(head *ListNode) *ListNode {
 	header := &ListNode{}
 	for tmp := header; head != nil; {
 		if tmp == header || tmp.Val != head.Val {

c2a85eb2acae65b8b2221fac2f4f69e8d14f8d14
seroiusgong <1393807014@qq.com>
2021-04-04 22:04:58 +0800

    day4

--- year2021/m4/day4.go
+++ year2021/m4/day4.go
@@ -7,11 +7,7 @@ func numRabbits(answers []int) int {
 	}
 	for i := 0; i < len(alphet); i++ {
 		if alphet[i] != 0 {
-			num := (alphet[i]) / (i + 1)
-			if (alphet[i])%(i+1) != 0 {
-				num++
-			}
-			res += num * (i + 1)
+			res += (alphet[i] + i) / (i + 1) * (i + 1)
 		}
 	}
 	return res

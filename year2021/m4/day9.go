5bc7985d0e328f1c6c057f5897335a187630949c
gc <chankungdeveloper@gmail.com>
2021-04-10 09:08:33 +0800

    day9

--- year2021/m4/day9.go
+++ year2021/m4/day9.go
@@ -1,5 +1,23 @@
 package m4
 
 func findMin(nums []int) int {
-
+	i, j := 0, len(nums)-1
+	for i < j {
+		// 因为原先是个升序含重复数组，有无论怎么旋转，如果[0]元素<[last]元素，那么 该数组即原数组
+		if nums[i] == nums[j] && nums[j-1] <= nums[j] {
+			i++
+			j--
+			continue
+		}
+		mid := (i + j) / 2
+		if nums[mid] <= nums[j] {
+			j = mid
+		} else {
+			i = mid + 1
+		}
+	}
+	if nums[i] > nums[j] {
+		return nums[j]
+	}
+	return nums[i]
 }

00b8c46018930fcae29d8a18819f9958e7c03bea
gc <chankungdeveloper@gmail.com>
2021-04-10 09:09:31 +0800

    day9简化

--- year2021/m4/day9.go
+++ year2021/m4/day9.go
@@ -3,7 +3,6 @@ package m4
 func findMin(nums []int) int {
 	i, j := 0, len(nums)-1
 	for i < j {
-		// 因为原先是个升序含重复数组，有无论怎么旋转，如果[0]元素<[last]元素，那么 该数组即原数组
 		if nums[i] == nums[j] && nums[j-1] <= nums[j] {
 			i++
 			j--
@@ -16,8 +15,6 @@ func findMin(nums []int) int {
 			i = mid + 1
 		}
 	}
-	if nums[i] > nums[j] {
-		return nums[j]
-	}
+
 	return nums[i]
 }

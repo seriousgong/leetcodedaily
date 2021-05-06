e651b994338d7a4407a1dc75260b881034fe591c
gc <chankungdeveloper@gmail.com>
2021-05-06 10:15:42 +0800

    day30

---
+++ year2021/m4/day30.go
@@ -0,0 +1,10 @@
+package m4
+
+func decode(encoded []int, first int) []int {
+	res := make([]int, len(encoded)+1)
+	res[0] = first
+	for i := 0; i < len(encoded); i++ {
+		res[i+1] = encoded[i] ^ res[i]
+	}
+	return res
+}
